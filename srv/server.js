793867f55ab125fc3babec1132fe915fa3c9fa0e
Jhodel Cailan <jhodel.cailan@gmail.com>
2023-05-07 18:07:58 +0800

    Updated module dependencies

--- srv/server.js
+++ srv/server.js
@@ -1,10 +1,6 @@
 const cds = require("@sap/cds");
 const cdsx = require("@rizing/cds-extension");
 
-cds.on("served", services => {
-	cdsx.load(services);
-});
-
 cds.on("bootstrap", () => {
-	cdsx.config();
+	cdsx.load({ config: true });
 });
